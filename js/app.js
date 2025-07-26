a1f02b926a8b58a97ec31365aa94c452a082927d
GolDRoger21 <ercan21@gmail.com>
2025-07-26 23:35:20 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,2 +0,0 @@
-// Site genelinde çalışacak olan kodlar (örneğin mobil menü açma/kapama) buraya eklenecektir.
-console.log("Adalet GYS Portalı ana script dosyası yüklendi.");
--- js/deneme.js
+++
@@ -1,1 +0,0 @@
-// Deneme sınavlarının interaktif mantığı (süre sayacı, soru geçişleri vb.) bu dosyada yer alacak.
